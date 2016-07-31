fdcbe23c253ac5cc7516d7780a3a1b8f13f9c18e
Neil Haeems <neilhaeems@berkeley.edu>
2016-07-30 18:48:39 -0700

    made code work as function

--- beautifulsoup_install.py
+++ beautifulsoup_install.py
@@ -1,7 +1,7 @@
-def html_text("html_code"):
+def convert_text(html_code):
     import requests
     from bs4 import BeautifulSoup
-    response = requests.get("html_code")
+    response = requests.get(html_code)
     txt = response.text
     soup = BeautifulSoup(txt, 'html.parser')
     print(soup.get_text())
